a2f5fd1e8501ebbdcea5e0a1406f10426d5985cf
spartanhero93 <spartanhero93@gmail.com>
2019-01-28 21:55:16 -0500

    added simple app with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export interface Props {
+  name: string
+  effort?: number
 }
 
-export default App;
+interface State {
+  currentEffort: number
+}
+
+const getExclamationMarks = (numChars: number) => Array(numChars + 1).join('!')
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      currentEffort: props.effort || 1,
+    }
+  }
+
+  updateEffort(currentEffort: number) {
+    this.setState({ currentEffort })
+  }
+
+  onIncrease = () => this.updateEffort(this.state.currentEffort + 1)
+  onDecrease = () => this.updateEffort(this.state.currentEffort - 1)
+
+  render() {
+    const { name } = this.props
+    const { currentEffort } = this.state
+
+    if (currentEffort <= 0) {
+      throw new Error('Increase your effort!')
+    }
+
+    return (
+      <div>
+        <h1>Hello {name + getExclamationMarks(currentEffort)}</h1>
+
+        <div>
+          <button onClick={this.onIncrease}>+</button>
+          <button onClick={this.onDecrease}>-</button>
+        </div>
+      </div>
+    )
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface Props {
+  name: string
+  effort?: number
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+interface State {
+  currentEffort: number
+}
+
+const getExclamationMarks = (numChars: number) => Array(numChars + 1).join('!')
+
+class App extends React.Component<Props, State> {
+  state = {
+    currentEffort: this.props.effort || 1,
+  }
+
+  updateEffort(currentEffort: number) {
+    this.setState({ currentEffort })
+  }
+
+  onIncrease = () => this.updateEffort(this.state.currentEffort + 1)
+  onDecrease = () => this.updateEffort(this.state.currentEffort - 1)
+
+  render() {
+    const { name } = this.props
+    const { currentEffort } = this.state
+
+    if (currentEffort <= 0) {
+      throw new Error('Increase your effort!')
+    }
+
+    return (
+      <div style={{ border: '2px solid', margin: '0 1rem', padding: '1rem' }}>
+        <h1>Hello {name + getExclamationMarks(currentEffort)}</h1>
+
+        <div>
+          <button onClick={this.onIncrease}>+</button>
+          <button onClick={this.onDecrease}>-</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+// class Root extends React.Component {
+//   state = {
+//     text: '',
+//     Family: ['Maria', 'Jose', 'Luis', 'Sarah', 'Carla'],
+//   }
+
+//   handleChange = (event: any) => this.setState({ text: event.target.value })
+//   addMember = () =>
+//     this.setState({ Family: [...this.state.Family, this.state.text], text: '' })
+
+//   render() {
+//     const { Family } = this.state
+//     return (
+//       <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+//         {Family.map(members => (
+//           <App name={members} />
+//         ))}
+
+//         <div style={{ position: 'absolute', bottom: '1rem' }}>
+//           <input
+//             onKeyPress={event => (event.key === 'Enter' ? this.addMember() : 'sad')}
+//             type='text'
+//             onChange={this.handleChange}
+//           />
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+ReactDOM.render(<App name='Luis' />, document.getElementById('root') as HTMLElement)
+
+serviceWorker.unregister()
