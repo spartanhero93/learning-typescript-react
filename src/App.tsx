9cecc7c1887a0c991258be4396516781a504c094
spartanhero93 <spartanhero93@gmail.com>
2022-12-24 00:20:18 -0500

    simple todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [input, handleInput] = useState<string>("");
+  const [todoArray, handleTodo] = useState<string[]>([]);
+
+  const handleChangeEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleInput(e.currentTarget.value);
+  };
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleTodo([...todoArray, input]);
+      handleInput("");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +23,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      <ul>{todoArray.length ? todoArray.map((i) => <li>{i}</li>) : ""}</ul>
+      <input type="text" onChange={handleChangeEvent} onKeyDown={handleKeyPress} value={input} />
+      <h2>{input}</h2>
     </div>
   );
 }
