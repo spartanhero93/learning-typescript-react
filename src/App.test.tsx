436f722cad8b0689c5fc0fde1355ea232e7630fc
spartanhero93 <spartanhero93@gmail.com>
2022-12-23 23:31:05 -0500

    using react 2022

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,29 +1,9 @@
-import * as React from 'react'
-import Enzyme from 'enzyme'
-import App from './App'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-it('renders the correct text when no effort level is giver', () => {
-  const hello = Enzyme.shallow(<App name='Luis' />)
-  expect(hello.find('h1').text()).toEqual('Hello Luis!')
-})
-it('renders the correct text with an explicit enthusiasm of 1', () => {
-  const hello = Enzyme.shallow(<App name='Luis' effort={1} />)
-  expect(hello.find('h1').text()).toEqual('Hello Luis!')
-})
-
-it('renders the correct text with an explicit enthusiasm level of 5', () => {
-  const hello = Enzyme.shallow(<App name='Luis' effort={5} />)
-  expect(hello.find('h1').text()).toEqual('Hello Luis!!!!!')
-})
-
-it('throws when the enthusiasm level is 0', () => {
-  expect(() => {
-    Enzyme.shallow(<App name='Luis' effort={0} />)
-  }).toThrow()
-})
-
-it('throws when the enthusiasm level is negative', () => {
-  expect(() => {
-    Enzyme.shallow(<App name='Luis' effort={-1} />)
-  }).toThrow()
-})
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,26 @@
-import React, { useReducer } from 'react'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-type StateType = {
-  todos: any,
-  text: any
-}
-type ActionType = {
-  type: 'ADD_TODO' | 'REMOVE_TODO' | 'EDIT_TODO' | "HANDLE_TEXT",
-  payload: string
-}
-
-
-const initialState = {
-  todos: [],
-  text: ''
-}
-
-function reducer(state: any, action: ActionType) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [...state.todos, state.text]
-    case 'HANDLE_TEXT':
-      return { text: action.payload }
-    default:
-      return state;
-  }
-}
-
-
-
-function App({ initialTodos = ['hello'], initialText = '' }) {
-  const [state, dispatch] = useReducer(reducer, { todos: initialTodos, text: initialText })
+function App() {
   return (
-    <>
-      <div>
-        Todos: {state.todos.map((i: string) => <div>{i}</div>)}
-      </div>
-      <h4>Current text: {state.text}</h4>
-      <input onChange={e => dispatch({ type: 'HANDLE_TEXT', payload: e.target.value })} type='text' />
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-// type StateType = {
-//   count: number
-// }
-
-// type ActionType = {
-//   type: 'reset' | 'decrement' | 'increment'
-// }
-
-// const initialState = { count: 0 }
-
-// function reducer(state: StateType, action: ActionType) {
-//   switch (action.type) {
-//     case 'reset':
-//       return initialState
-//     case 'increment':
-//       return { count: state.count + 1 }
-//     case 'decrement':
-//       return { count: state.count - 1 }
-//     default:
-//       return state
-//   }
-// }
-
-// function App({ initialCount = 1 }) {
-//   const [state, dispatch] = useReducer(reducer, { count: initialCount })
-//   return (
-//     <>
-//       Count: {state.count}
-//       <button onClick={() => dispatch({ type: 'increment' })}>Add</button>
-//       <button onClick={() => dispatch({ type: 'decrement' })}>Dec</button>
-//     </>
-//   )
-// }
-
-export default App
+export default App;
